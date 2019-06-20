0a4c59f28ce7309ec216af61fac377f2b1fd33de
dongxy7 <43315437+dongxy7@users.noreply.github.com>
2019-06-20 14:30:47 +0800

    修改绑定交互

--- LittleMiser/personInfo/personalInfo.js
+++ LittleMiser/personInfo/personalInfo.js
@@ -1,150 +1,154 @@
+ $(document).ready(function(){
+  var data = {};
+  // 读取JSON数据，存入expresses数组中
+  per.personalInfo();
+  per.user_name= per.new_name ='d';
+  per.money='100';
+  per.real_name='张三';
+  per.number='16340000';
+  per.age=per.new_age='20';
+  per.gender='女';
+  per.grade=per.new_grade='2016级';
+  per.major=per.new_major='软件工程';
+  per.intro=per.new_intro='abcdefg,hijklmn,opq,rs,uvwxyz!!!abcdefg,hijklmn,opq,rs,uvwxyz!!!';
+  per.correctPasswd='123456';
 
-function changePasswd() {
-    var x = document.getElementById("personalInfo");
-    x.innerHTML = 
-        "<div id=\"change\">\
-            <div  id=\"change_pass\">\
-                <form autocomplete=\"on\" action=\"handleChangePasswd\" method=\"POST\">\
-                    <div id=\"change_passwd\" class=\"ui form\">\
-                        <hr/>\
-                        <h5 >修改密码</h5>\
-                        <hr/>\
-                        <div style=\"height: 200px;\">\
-                            <div class=\"field\">\
-                                <label for=\"inputAccount\" class=\"sr-only\" style=\"font-size:17px\">当前密码</label>\
-                                <input type=\"password\" id=\"currPasswd\" placeholder=\"当前密码\" name=\"currPasswd\" class=\"form-control\" required=\"required\" autofocus/><br/>\
-                            </div>\
-                            <div class=\"field\">\
-                                <label for=\"newPasswd\" class=\"sr-only\" style=\"font-size:17px\">新密码</label>\
-                                <input type=\"password\" id=\"newPassword\" placeholder=\"新密码\" name=\"newPasswd\" class=\"form-control\" required/><br/>\
-                            </div>\
-                            <div class=\"field\">\
-                                <label for=\"repeatPasswd\" class=\"sr-only\" style=\"font-size:17px\">确认密码</label>\
-                                <input type=\"password\" id=\"repeatPasswd\" placeholder=\"确认密码\" name=\"repeatPasswd\" class=\"form-control\" required/><br/>\
-                            </div>\
-                        </div>\
-                        <div id=\"double_btn2\" align=\"center\">\
-                        <div class=\"ui buttons\" >\
-                            <button class=\"ui positive button\" type=\"submit\">修改密码</button>\
-                                <div class=\"or\"></div>\
-                                <button class=\"ui button\" onclick=\"personalInfo()\">取消</button>\
-                            </div>\
-                        </div>\
-                    </div>\
-                </form>\
-            </div>\
-        </div>";
-}
+});
 
-function personalInfo() {
-    var x = document.getElementById("change");
-    x.innerHTML = 
-        "<form class=\"ui form\" id=\"all_info\">\
-            <div id=\"seven_item\">\
-                <hr/>\
-                <h5 id=\"info_header\">个人信息</h5>\
-                <hr/>\
-                <div style=\"height: 200px;\">\
-                    <div class=\"field\" id=\"items\">\
-                        <div class=\"two fields\">\
-                            <div class=\"field\">\
-                                <div class=\"field\" id=\"user_name\">用户名:name</div>\
-                            </div>\
-                            <div class=\"field\">\
-                                <div class=\"field\">邮箱：123456@qq.com</div>\
-                            </div>\
-                        </div>\
-                    </div>\
-                    <div class=\"field\" id=\"items\">\
-                        <div class=\"two fields\">\
-                            <div class=\"field\">\
-                                <div class=\"field\">姓名：赵钱孙李</div>\
-                            </div>\
-                            <div class=\"field\">\
-                                <div class=\"field\">学号：16340000</div>\
-                            </div>\
-                        </div>\
-                    </div>\
-                    <div class=\"field\" id=\"items\">\
-                        <div class=\"two fields\">\
-                            <div class=\"field\">\
-                                <div class=\"field\">年龄：20</div>\
-                            </div>\
-                            <div class=\"field\">\
-                                <div class=\"field\">性别：女</div>\
-                            </div>\
-                        </div>\
-                    </div>\
-                    <div class=\"field\" id=\"items\">\
-                        <div class=\"two fields\">\
-                            <div class=\"field\">\
-                                <div class=\"field\">年级：2016级</div>\
-                            </div>\
-                            <div class=\"field\">\
-                                <div class=\"field\">专业：软件工程</div>\
-                            </div>\
-                        </div>\
-                    </div>\
-                    <div class=\"field\" id=\"intro\">简介：abcdefg,hijklmn,opq,rs,uvwxyz!!!abcdefg,hijklmn,opq,rs,uvwxyz!!!</div>\
-                </div>\
-                <a class=\"ui inverted blue button\" onclick=\"changePersonalInfo()\" id=\"changeInfoBtn\" >修改个人信息</a>\
-            </div>\
-            <div id=\"double_btn\" align=\"center\">\
-                <div class=\"ui buttons\">\
-                    <button class=\"ui button\" onclick=\"changePasswd()\">修改密码</button>\
-                    <div class=\"or\"></div>\
-                    <button class=\"ui positive button\" onclick=\"personalInfo()\">个人信息</button>\
-                </div>\
-            </div>\
-        </form>";
-}
-function changePersonalInfo() {
-    var x = document.getElementById("personalInfo");
-    x.innerHTML =  
-        "<div id=\"change\">\
-            <div id=\"change_info\">\
-                <form autocomplete=\"on\" action=\"handleChangeInfo\" method=\"POST\" >\
-                    <div id=\"change_information\" class=\"ui form\">\
-                        <hr/>\
-                        <h5>修改个人信息</h5>\
-                        <hr/>\
-                        <div class=\"field\" id=\"items\">\
-                            <div class=\"two fields\">\
-                                <div class=\"field\">\
-                                    <label for=\"userName\" class=\"sr-only\" style=\"font-size:15px\">用户名</label>\
-                                    <input type=\"text\" id=\"nicknamed\" class=\"form-control\" name=\"nicknamed\" value=\"name\" /><br/>\
-                                </div>\
-                                <div class=\"field\">\
-                                    <label for=\"email\" class=\"sr-only\" style=\"font-size:15px\">邮箱</label>\
-                                    <input type=\"text\" id=\"email\" class=\"form-control\" name=\"email\" value=\"1234567@qq.com\"/><br/>\
-                                </div>\
-                            </div>\
-                        </div>\
-                        <div class=\"field\" id=\"items\">\
-                            <div class=\"two fields\">\
-                                <div class=\"field\">\
-                                    <label for=\"grade\" class=\"sr-only\" style=\"font-size:15px\">年级</label>\
-                                    <input type=\"text\" id=\"grade\" class=\"form-control\" name=\"grade\" value=\"2016级\"/><br/>\
-                                </div>\
-                                <div class=\"field\">\
-                                    <label for=\"major\" class=\"sr-only\" style=\"font-size:15px\">专业</label>\
-                                    <input type=\"text\" id=\"major\" class=\"form-control\" name=\"major\" value=\"软件工程\"/><br/>\
-                                </div>\
-                            </div>\
-                        </div>\
-                        <div class=\"field\" id=\"intro\">\
-                            <label for=\"introduction\" class=\"sr-only\"style=\"font-size:15px\">简介</label>\
-                            <input type=\"text\" id=\"introduction\" class=\"form-control\" name=\"introduction \" value=\"abcdefg,hijklmn,opq,rs,uvwxyz!!!abcdefg,hijklmn,opq,rs,uvwxyz!!!abcdefg,hijklmn,opq,rs,uvwxyz!!!abcdefg,hijklmn,opq,rs,uvwxyz!!!\"/><br/>\
-                        </div>\
-                    </div>\
-                    <div id=\"double_btn3\" align=\"center\">\
-                        <div class=\"ui buttons\" >\
-                            <button class=\"ui positive button\" type=\"submit\">修改信息</button>\
-                            <div class=\"or\"></div>\
-                            <button class=\"ui button\" onclick=\"personalInfo('changeInfo')\">取消</button>\
-                        </div>\
-                    </div>\
-                </form>\
-            </div>\
-        </div>";
-}  
+var per = new Vue({ 
+    el: '#box',
+    data: {
+        user_name: '',
+        money: '',
+        real_name: '',
+        number: '',
+        age: '',
+        gender:'',
+        grade:'',
+        major:'',
+        intro:'',
+        correctPasswd:'',
+
+        oldPasswd:'',
+        newPasswd :'',
+        confirmPasswd:'',
+        new_name:'',
+        new_age:'',
+        new_grade:'',
+        new_major:'',
+        new_intro:'',
+
+        left_button:'修改密码',
+        right_button:'个人信息',
+
+        passwordMsg : ''
+    },
+
+    methods: {
+        left_button_click:function() {
+            if(this.left_button == '修改密码') {
+                this.changePasswd();
+            }else if (this.left_button == '提交密码') {
+                this.submit_changePasswd();
+            }else {
+                this.submit_changePersonalInfo();
+            }
+        },
+        right_button_click:function() {
+            this.personalInfo();
+        },
+        personalInfo:function() {
+            $('#change_pass').hide();
+            $('#change_info').hide();
+            $('#seven_item').show();
+            this.right_button = '个人信息';
+            this.left_button = '修改密码';
+        },
+        changePersonalInfo:function() {
+            $('#change_pass').hide();
+            $('#change_info').show();
+            $('#seven_item').hide();
+            this.right_button = '取消';
+            this.left_button = '提交信息';
+            this.new_name = this.user_name;
+            this.new_age = this.age;
+            this.new_grade = this.grade;
+            this.new_major = this.major;
+            this.new_intro = this.intro;
+        },
+        changePasswd:function() {
+            $('#change_pass').show();
+            $('#change_info').hide();
+            $('#seven_item').hide();
+            this.right_button = '取消';
+            this.left_button = '提交密码';
+            this.oldPasswd = '';
+            this.newPasswd = '';
+            this.confirmPasswd = '';
+        },
+        submit_changePasswd:function() {
+            if(this.oldPasswd == this.correctPasswd) {
+                if(this.checkPassword()) {
+                    if(this.newPasswd == this.confirmPasswd) {
+                        this.correctPasswd = this.newPasswd;
+
+
+
+                        //数据库修改密码
+                        //TODO
+                        //
+
+
+                        alert("修改密码成功");
+                        this.personalInfo();
+                    }
+                    else {
+                        alert("确认密码与新密码不符，请重新确认");
+                        this.oldPasswd = '';
+                        this.newPasswd = '';
+                        this.confirmPasswd = '';
+                    }
+                }
+                else {
+                    alert("密码格式不符，请重新输入！");
+                    this.oldPasswd = '';
+                    this.newPasswd = '';
+                    this.confirmPasswd = '';
+                }
+            }
+            else {
+                alert("原始密码错误，请重新输入！");
+                this.oldPasswd = '';
+                this.newPasswd = '';
+                this.confirmPasswd = '';
+            }
+        },
+        submit_changePersonalInfo:function() {
+            this.user_name = this.new_name;
+            this.age = this.new_age;
+            this.grade = this.new_grade;
+            this.major = this.new_major;
+            this.intro = this.new_intro;
+
+
+            //数据库修改信息
+            //TODO
+            //
+
+
+            alert("修改个人信息成功");
+            this.personalInfo();
+        },
+        checkPassword:function(){
+            if(!/^(\w){6,20}$/.test(this.newPasswd)){
+                this.passwordMsg = "密码长度为6-20，可包括字母、数字和下划线";
+                return false;
+            }
+            else{
+                this.passwordMsg = '';
+                return true;
+            }
+        }
+    }
+});
+
